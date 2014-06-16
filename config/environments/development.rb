9bde2fc3e3fb9f8dbcee1d8cc9e10ef689e1a587
guy cao <guycao@gmail.com>
2014-06-16 13:18:37 -0700

    change app name in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-McapiRubyExample::Application.configure do
+BlocMail::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
