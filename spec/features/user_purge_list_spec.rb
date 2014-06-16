0df894c99e24b6912796aa73f5242ecb97e10676
guy cao <guycao@gmail.com>
2014-06-16 11:30:47 -0700

    remove save and open page

--- spec/features/user_purge_list_spec.rb
+++ spec/features/user_purge_list_spec.rb
@@ -8,7 +8,7 @@ feature "User purges list and removes old emails" do
     setup_mc(list_id, "list", email_address)
     # visit lists_path, '123'
     visit "/lists/#{list_id}"
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content(email_address)
     click_button 'Purge List'
     expect(page).to have_content("succesfully unsubscribed 1 email(s)")
