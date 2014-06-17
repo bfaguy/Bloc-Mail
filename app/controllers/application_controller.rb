a83497ce4b5b8cc356137afc1268d08237d89cd5
guy cao <guycao@gmail.com>
2014-06-17 14:08:12 -0700

    un-ignored application_controller and commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :setup_mcapi
 
   def setup_mcapi
-    @mc = Mailchimp::API.new('00000000000000000000000000000000-us1')
+    @mc = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
+    @gibbon_export = Gibbon::Export.new(ENV['MAILCHIMP_API_KEY'])
   end
 end
