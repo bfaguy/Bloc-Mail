f784736e3b11a63e7be9217460b8870e312a11f6
guy cao <guycao@gmail.com>
2014-06-17 17:42:22 -0700

    added users. tested sign in and out

---
+++ spec/support/user_helper.rb
@@ -0,0 +1,17 @@
+module  UserHelper
+
+  def login(a)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: a.email
+    fill_in 'Password', with: a.password
+    click_button 'Sign in'
+  end
+
+  def logout(a)
+    visit root_path
+    click_link 'Sign Out'
+  end
+
+end
+
