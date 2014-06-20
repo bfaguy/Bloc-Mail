f18df5097dc5b4e5fd30d676000c93d9d8488b05
guy cao <guycao@gmail.com>
2014-06-20 16:56:11 -0700

    tested logging purges

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -8,16 +8,26 @@ describe ListsController do
     sign_in user
   end
 
-  describe "#purge" do
+  describe "POST #purge" do
     it "removes any old emails address from the list succesfully" do
       list_id = '123'
       double_mc = mock_mc("new list", list_id)
       mock_gibbon_list(3)
 
-      # expect_any_instance_of(Mailchimp::Lists).to receive(:unsubscribe).once
       expect(double_mc.lists).to receive(:unsubscribe).once
       post :purge, :id => list_id
     end
+
+    it "logs the purges in database" do
+      list_id = '123'
+      mock_mc("new list", list_id)
+      mock_gibbon_list(3)
+
+      expect{ 
+        post :purge, :id => list_id
+      }.to change(Purge,:count).by(1) 
+    end
+
   end
 
 end
